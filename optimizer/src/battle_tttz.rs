2c0b438b8febcf7ee3829a82f6b10d3b2fe555f0
asrcpq <asrcpq@outlook.com>
2021-03-19 02:58:58 +0000

    bug fix

--- optimizer/src/battle_tttz.rs
+++ optimizer/src/battle_tttz.rs
@@ -1,7 +1,6 @@
 use tttz_mpboard::Game;
 use tttz_ai::CCBot;
 use tttz_ai::Thinker;
-use cold_clear::Interface;
 use cold_clear::evaluation::Standard;
 
 pub fn do_battle(
@@ -9,29 +8,17 @@ pub fn do_battle(
 ) -> Option<((), bool)> {
 	let mut game = Game::new(1, 2, [].iter());
 	let mut bots = [
-		CCBot {
-			interface: Interface::launch(
-				libtetris::Board::new(),
-				Default::default(),
-				p1,
-				None,
-			),
-			preview_list: [7; 6],
-		},
-		CCBot {
-			interface: Interface::launch(
-				libtetris::Board::new(),
-				Default::default(),
-				p2,
-				None,
-			),
-			preview_list: [7; 6],
-		},
+		CCBot::from_eval(p1),
+		CCBot::from_eval(p2),
 	];
 	let mut player = 0; // current player index
 	loop {
 		let display = game.generate_display(player, 0);
-		for key_type in bots[player].main_think(display).into_iter() {
+		let ret = bots[player].main_think(display);
+		if ret.is_empty() {
+			return Some(((), player == 2))
+		}
+		for key_type in ret.into_iter() {
 			let ret = game.process_key(player as i32 + 1, 0, key_type).0;
 			if ret > 0 {
 				return Some(((), ret == 1))
